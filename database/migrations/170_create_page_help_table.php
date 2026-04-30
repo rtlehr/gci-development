8fd8d217115830a164642baefea2ff3a0fafb7d9
Ross Lehr <itsme@rosslehr.com>
2026-04-30 08:00:39 -0400

    Edits to Migrate files

--- database/migrations/170_create_page_help_table.php
+++ database/migrations/170_create_page_help_table.php
@@ -30,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('page_help');
+        //Schema::dropIfExists('page_help');
     }
 };
