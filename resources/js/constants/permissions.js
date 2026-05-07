80bbc41c6c865d14a3d25d7221f2e073a60c8809
Ross Lehr <itsme@rosslehr.com>
2026-05-07 13:41:04 -0400

    updated permision seeder

--- resources/js/constants/permissions.js
+++ resources/js/constants/permissions.js
@@ -26,6 +26,32 @@ export const Permissions = {
     TICKETS_CREATE: 'create_tickets',
     TICKETS_READ: 'read_tickets',
     TICKETS_UPDATE: 'update_tickets',
-    TICKETS_DELETE: 'delete_tickets'
+    TICKETS_DELETE: 'delete_tickets',
+
+    //stoped here
+
+    GROUPS_ACCESS: 'access_groups',
+    GROUPS_CREATE: 'create_groups',
+    GROUPS_READ: 'read_groups',
+    GROUPS_UPDATE: 'update_groups',
+    GROUPS_DELETE: 'delete_groups',
+
+    TEAMS_ACCESS: 'access_teams',
+    TEAMS_CREATE: 'create_teams',
+    TEAMS_READ: 'read_teams',
+    TEAMS_UPDATE: 'update_teams',
+    TEAMS_DELETE: 'delete_teams',
+
+    PERMISSIONS_ACCESS: 'access_permissions',
+    PERMISSIONS_CREATE: 'create_permissions',
+    PERMISSIONS_READ: 'read_permissions',
+    PERMISSIONS_UPDATE: 'update_permissions',
+    PERMISSIONS_DELETE: 'delete_permissions',
+
+    ROLES_ACCESS: 'access_roles',
+    ROLES_CREATE: 'create_roles',
+    ROLES_READ: 'read_roles',
+    ROLES_UPDATE: 'update_roles',
+    ROLES_DELETE: 'delete_roles',
 
 }
